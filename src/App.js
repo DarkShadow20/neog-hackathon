59f0c6069bab6f77a6ab45f63ce3aa20efaec7c6
DarkShadow20 <kunal8011@gmail.com>
2021-05-01 14:43:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" className="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
